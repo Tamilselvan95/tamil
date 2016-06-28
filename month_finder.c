aae8f88b5533999dcf21af4e0529bcc88062c419
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-28 16:27:48 +0530

    Create month_finder.c

---
+++ month_finder.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int date,month,year;
+clrscr();
+printf("enter the format(dd-mm-yyyy):\n");
+scanf("%d-%d-%d",&date,&month,&year);
+switch(month)
+{
+case 1:
+printf("January");
+break;
+case 2:
+printf("February");
+break;
+case 3:
+printf("March");
+break;
+case 4:
+printf("April");
+break;
+case 5:
+printf("May");
+break;
+case 6:
+printf("June");
+break;
+case 7:
+printf("July");
+break;
+case 8:
+printf("August");
+break;
+case 9:
+printf("September");
+break;
+case 10:
+printf("October");
+break;
+case 11:
+printf("November");
+break;
+case 12:
+printf("December");
+break;
+}
+getch();
+}
+

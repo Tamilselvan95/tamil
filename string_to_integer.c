545c07cc080d8400e07f7788622cfe9bd48ec681
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-28 13:45:53 +0530

    Create string_to_integer.c

---
+++ string_to_integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+char a[100];
+int b;
+clrscr();
+printf("Enter the value:\n");
+scanf("%s",a);
+b=atoi(a);
+printf("the integer value is :%d\n",b);
+getch();
+}

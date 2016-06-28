e8307819711dc2ed16d3db1a294a56b81fec1e80
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-28 11:20:46 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 void main()
 {
-	  char a[200];
-	  int b=0,i;
-	  clrscr();
-	  printf("Enter the string \n");
-	  gets(a);
-	  for(i=0;a[i]!='\0';++i)
-	  {
-	  if(a[i]=='a'||a[i]=='A'||a[i]=='e'||a[i]=='E'||a[i]=='i'||a[i]=='I'||a[i]=='o'||a[i]=='O'||a[i]=='u'||a[i]=='U')
-	  ++b;
-	  }
-	  printf("NUmber of vowels in %s is %d",a,b);
-	  getch();
+char a[200];
+int b=0,i;
+clrscr();
+printf("Enter the string \n");
+gets(a);
+for(i=0;a[i]!='\0';++i)
+{
+if(a[i]=='a'||a[i]=='A'||a[i]=='e'||a[i]=='E'||a[i]=='i'||a[i]=='I'||a[i]=='o'||a[i]=='O'||a[i]=='u'||a[i]=='U')
+++b;
+}
+printf("Number of vowels in %s is %d",a,b);
+getch();
 	  }

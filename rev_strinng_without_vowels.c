232f4b956bfe39e314a760e9cb16dd441bed1cea
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-29 10:48:44 +0530

    Update rev_strinng_without_vowels.c

--- rev_strinng_without_vowels.c
+++ rev_strinng_without_vowels.c
@@ -4,17 +4,20 @@ void main()
 {
 char a[100],b[100];
 int i,j=0;
-printf("Enter the string  " );
+clrscr();
+/*printf("Enter the string \n " );*/
 gets(a);
 strrev(a);
+printf("%s\n",a);
 for(i=0;a[i]!='\0';++i)
 {
 if((a[i]=='a'||a[i]=='A'||a[i]=='e'||a[i]=='E'||a[i]=='i'||a[i]=='I'||a[i]=='o'||a[i]=='O'||a[i]=='u'||a[i]=='U')==0)
 {
-  b[j]=a[i];
+b[j]=a[i];
 j++;
 }
 }
-printf("Reversed string without vowel:  %s ",b);
+b[j]='\0';
+printf("%s",b);
 getch();
 }

49301dc692da70638917a9df722797fe65dc3819
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-30 13:34:34 +0530

    Update sum_1st_last_digits.c

--- sum_1st_last_digits.c
+++ sum_1st_last_digits.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void main()
 {
-int a,b, sum=0;
+int a,b,sum=0;
 clrscr();
 printf("Enter the number to add 1st and last digit : " );
 scanf("%d",&a);
 if(a>10)
 {
-sum +=a%10;
+sum+=a%10;
 }
 while(a>=10)
 {

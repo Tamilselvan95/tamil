dc3934e50c956ff07b97830dc6b5957a6a5d6974
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-30 13:44:24 +0530

    Create sum_odd.c

---
+++ sum_odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,i,sum=0;
+clrscr();
+printf("Enter the number ");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+if(i%2!=0)
+{
+sum+=i;
+}
+}
+printf("Sum of odd digits in the range = %d",sum);
+getch();
+}

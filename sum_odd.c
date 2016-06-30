24b8a0c1ecb6bb0fe5730a456e8a7f727b134633
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-30 13:48:29 +0530

    Update sum_odd.c

--- sum_odd.c
+++ sum_odd.c
@@ -5,6 +5,8 @@ int a,i,sum=0;
 clrscr();
 printf("Enter the number ");
 scanf("%d",&a);
+if(a>0)
+{
 for(i=1;i<=a;i++)
 {
 if(i%2!=0)
@@ -12,6 +14,9 @@ if(i%2!=0)
 sum+=i;
 }
 }
+}
+else
+sum=0;
 printf("Sum of odd digits in the range = %d",sum);
 getch();
 }

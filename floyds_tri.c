61e1311fe07c423c786d0dfeccdab30668ac5e18
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-06-30 14:42:47 +0530

    Update floyds_tri.c

--- floyds_tri.c
+++ floyds_tri.c
@@ -4,13 +4,13 @@ void main()
 {
 int a,i,j,b=0;
 clrscr();
-printf("number for floyds triangle\n");
+printf("Floyds triangle value\n");
 scanf("%d",&a);
 for(i=1;i<=a;i++)
 {
 for(j=1;j<=i;j++)
 {
- b++ ;
+b++;
 printf("%d\t",b);
 }
 printf("\n");

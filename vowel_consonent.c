feee1bee0059455660de632412508ffb9130fe13
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-07-03 14:12:08 +0530

    Create vowel_consonent.c

---
+++ vowel_consonent.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+scanf("%c",&a);
+if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
+printf("Vowels");
+else
+printf("Consonent");
+getch();
+}

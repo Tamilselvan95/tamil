a380e92f0e4defb99fcb258c3cfff0dd57999afa
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-07-04 11:31:34 +0530

    Create first_repeat.c

---
+++ first_repeat.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],n,i,j,c;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+c=0;
+for(j=0;j<n;j++)
+{
+if(a[i]==a[j])
+c++;
+}
+if(c>1)
+printf("%d",a[i]);
+break;
+}
+getch();
+}

46cc54d096322e720165c975cdadf3ec15873660
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-07-04 11:47:22 +0530

    Create add2_get1.c

---
+++ add2_get1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],n,i,j,k;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(i!=j)
+{
+for(k=0;k<n;k++)
+{
+if(a[i]+a[j]==a[k])
+printf("%d+ %d= %d\n",a[i],a[j],a[k]);
+}
+}
+}
+}
+getch();
+}

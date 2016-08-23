9b14b07d6aedc4741479478829cc4f3c2f2d5984
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-08-23 15:00:12 +0530

    Create sum_fibnoc.java

---
+++ sum_fibnoc.java
@@ -0,0 +1,27 @@
+
+package tamil;
+
+import java.util.Scanner;
+
+public class fin {
+
+    public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+       int n=s.nextInt();
+       int first=0,next,second=1,sum=0;
+        for ( int c = 1 ; c <= n ; c++ )
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         next = first + second;
+         first = second;
+         second = next;
+      }System.out.println(next);
+      sum=sum+next;
+   }System.out.println(sum);         
+    
+    }
+}
+
